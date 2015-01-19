7410daffc54008e91c4377e0d868900ffe8cc5c9
Alvaro Pereira <alvaro@opendns.com>
2015-01-19 13:03:07 -0800

    Generic Juniper MX check, with option to manually set or automatically detect the number of routing engines. Supports MX5/MX10/MX40/MX80/MX240/MX480/MX960

--- check-scripts/check_mx.pl
+++ check-scripts/check_mx.pl
@@ -10,6 +10,7 @@ use Getopt::Std;
 # Some defaults
 #-------------------------------------------------------------------------------
 
+my $default_re_count = 1;
 my $default_temp_warning = 45;
 my $default_temp_critical = 55;
 my $default_mem_warning = 80;
@@ -22,11 +23,11 @@ my $default_cpu_critical = 90;
 # Check the usage
 #-------------------------------------------------------------------------------
 
-my ($hostname,$community,$owner, $temp_warning,$temp_critical,$mem_warning,$mem_critical,$cpu_warning,$cpu_critical);
+my ($hostname,$community,$owner,$re_count,$re_count_autodetect,$temp_warning,$temp_critical,$mem_warning,$mem_critical,$cpu_warning,$cpu_critical);
 
 check_usage();
 sub check_usage {
-	my $opt_string = 'H:C:t:T:m:M:h' ;
+	my $opt_string = 'H:C:r:t:T:m:M:h' ;
 	my %opt;
 	getopts( "$opt_string", \%opt );
 
@@ -48,6 +49,14 @@ sub check_usage {
 		exit 4;
 	} else {
 		$community = $opt{C};
+	}
+
+	if (defined $opt{r} && ($opt{r} == 1 || $opt{r} == 2)) {
+		$re_count = $opt{r};
+		$re_count_autodetect = 0;
+	} else {
+		$re_count = $default_re_count;
+		$re_count_autodetect = 1;
 	}
 
 	if (defined $opt{t} && $opt{t} >= 0) {
@@ -94,6 +103,8 @@ sub check_usage {
 # 	Set defaults
 #===============================================================================
 
+my $sysdescr_oid = "1.3.6.1.4.1.2636.3.1.2.0";
+
 my $mem_oid_re0 = "1.3.6.1.4.1.2636.3.1.13.1.11.9.1.0.0";
 my $mem_oid_re1 = "1.3.6.1.4.1.2636.3.1.13.1.11.9.2.0.0";
 
@@ -104,6 +115,13 @@ my $cpu_oid_re0 = "1.3.6.1.4.1.2636.3.1.13.1.8.9.1.0.0";
 my $cpu_oid_re1 = "1.3.6.1.4.1.2636.3.1.13.1.8.9.2.0.0";
 my $cpu_redundancy_state_oid_re0 = "1.3.6.1.4.1.2636.3.1.14.1.7.9.1.0.0";
 my $cpu_redundancy_state_oid_re1 = "1.3.6.1.4.1.2636.3.1.14.1.7.9.2.0.0";
+
+# first of all, if re_count not defined by the user, auto detect it
+if ($re_count_autodetect == 1) {
+	my @sysdescr_oids = ($sysdescr_oid);
+	my $snmp_result_sysdescr = get_snmp_result($hostname,$community,\@sysdescr_oids);
+	analyze_sysdescr($snmp_result_sysdescr);
+} 
 
 # start with memory
 my $mem_status_str = "";
@@ -159,6 +177,37 @@ else {
 #-------------------------------------------------------
 # Functions
 #-------------------------------------------------------
+sub analyze_sysdescr {
+	my $snmp_result = shift;
+	my $sysdescr = "";
+	my $sysdescr_status_str_0 = "";
+
+	# Now analyze
+	if ((!defined($snmp_result->{$sysdescr_oid})) || ($snmp_result->{$sysdescr_oid} eq "noSuchInstance")  || ($snmp_result->{$sysdescr_oid} eq "noSuchObject")) {
+		$sysdescr_status_str_0 = "Could not determine system description, $sysdescr_oid";
+	} else {
+		$sysdescr = lc($snmp_result->{$sysdescr_oid});
+
+		if (index($sysdescr, lc("MX5")) != -1) {
+			$re_count = 1;
+		} elsif (index($sysdescr, lc("MX10")) != -1) {
+			$re_count = 1;
+		} elsif (index($sysdescr, lc("MX40")) != -1) {
+			$re_count = 1;
+		} elsif (index($sysdescr, lc("MX80")) != -1) {
+			$re_count = 1;
+		} elsif (index($sysdescr, lc("MX240")) != -1) {
+			$re_count = 2;
+		} elsif (index($sysdescr, lc("MX480")) != -1) {
+			$re_count = 2;
+		} elsif (index($sysdescr, lc("MX960")) != -1) {
+			$re_count = 2;
+		} else {
+			$re_count = $default_re_count;
+		}
+	}
+}
+
 sub analyze_temperature {
 	my $snmp_result = shift;
 	my $temp_status_str_0 ="";
@@ -169,7 +218,7 @@ sub analyze_temperature {
 	my $temp_re1 = 0;
 
 	# Now analyze
-	if ((!defined($snmp_result->{$temp_oid_re0})) || ($snmp_result->{$temp_oid_re0} eq "noSuchInstance")) {
+	if ((!defined($snmp_result->{$temp_oid_re0})) || ($snmp_result->{$temp_oid_re0} eq "noSuchInstance") || ($snmp_result->{$temp_oid_re0} eq "noSuchObject")) {
 		$temp_status_str_0 = "Could not determine temperature usage for re0, $temp_oid_re0";
 		$temp_status_exit_0 =  1;
 	} else {
@@ -187,35 +236,43 @@ sub analyze_temperature {
 		}
 	}
 
-	if ((!defined($snmp_result->{$temp_oid_re1})) || ($snmp_result->{$temp_oid_re1} eq "noSuchInstance")) {
-		$temp_status_str_1 = "Could not determine temperature for re1, $temp_oid_re1";
-		$temp_status_exit_1 =  1;
-	} else {
-		$temp_re1 = $snmp_result->{$temp_oid_re1};
-
-		if ($temp_re1 >= $temp_critical ) {
-		 	$temp_status_exit_1 =  2;
-		} elsif ($temp_re1 >= $temp_warning) {
-		 	$temp_status_exit_1 =  1;
-		} elsif (($temp_re1 < $temp_critical) && ($temp_re1 < $temp_warning)) {
-		 	$temp_status_exit_1 =  0;
+	if($re_count == 2) {
+		if ((!defined($snmp_result->{$temp_oid_re1})) || ($snmp_result->{$temp_oid_re1} eq "noSuchInstance") || ($snmp_result->{$temp_oid_re1} eq "noSuchObject")) {
+			$temp_status_str_1 = "Could not determine temperature for re1, $temp_oid_re1";
+			$temp_status_exit_1 =  1;
 		} else {
-			$temp_status_str_1 = "Unknown: Something went wrong....\n";
-		 	$temp_status_exit_1 =  3;
+			$temp_re1 = $snmp_result->{$temp_oid_re1};
+
+			if ($temp_re1 >= $temp_critical ) {
+				$temp_status_exit_1 =  2;
+			} elsif ($temp_re1 >= $temp_warning) {
+				$temp_status_exit_1 =  1;
+			} elsif (($temp_re1 < $temp_critical) && ($temp_re1 < $temp_warning)) {
+				$temp_status_exit_1 =  0;
+			} else {
+				$temp_status_str_1 = "Unknown: Something went wrong....\n";
+				$temp_status_exit_1 =  3;
+			}
 		}
 	}
 
-
-	# Now evaluate results
-	$temp_status_str = "temperature RE0:".$temp_re0 ."c".$temp_status_str_0 ." RE1:".$temp_re1 ."c $temp_status_str_1";
-	$temp_perf_data = "temperature_re0 = $temp_re0, temperature_re1 = $temp_re1";
-	
-	if ($temp_status_exit_0 > $temp_status_exit_1) {
-		$temp_status_exit =  $temp_status_exit_0;
+	if($re_count == 2) {
+		# Now evaluate results for systems with 2 REs
+		$temp_status_str = "temperature RE0:".$temp_re0 ."c".$temp_status_str_0 ." RE1:".$temp_re1 ."c $temp_status_str_1";
+		$temp_perf_data = "temperature_re0 = $temp_re0, temperature_re1 = $temp_re1";
+		
+		if ($temp_status_exit_0 > $temp_status_exit_1) {
+			$temp_status_exit =  $temp_status_exit_0;
+		} else {
+			$temp_status_exit =  $temp_status_exit_1;
+		}
 	} else {
-		$temp_status_exit =  $temp_status_exit_1;
+		# Now evaluate results for systems with 1 RE
+		$temp_status_str = "temperature RE0:".$temp_re0 ."c ".$temp_status_str_0;
+		$temp_perf_data = "temperature_re0 = $temp_re0";
+		
+		$temp_status_exit =  $temp_status_exit_0;
 	}
-
 }
 
 sub analyze_memory_usage {
@@ -227,7 +284,7 @@ sub analyze_memory_usage {
 	my $mem_re0 = 0;
 	my $mem_re1 = 0;
 
-	if ((!defined($snmp_result->{$mem_oid_re0})) || ($snmp_result->{$mem_oid_re0} eq "noSuchInstance")) {
+	if ((!defined($snmp_result->{$mem_oid_re0})) || ($snmp_result->{$mem_oid_re0} eq "noSuchInstance") || ($snmp_result->{$mem_oid_re0} eq "noSuchObject")) {
 		$mem_status_str_0 = " Could not determine memory usage for re0, $mem_oid_re0 ";
 		$mem_status_exit_0 =  1;
 	} else {
@@ -246,33 +303,43 @@ sub analyze_memory_usage {
 	}
 
 
-	if ((!defined($snmp_result->{$mem_oid_re1})) || ($snmp_result->{$mem_oid_re1} eq "noSuchInstance")) {
-		$mem_status_str_1 =  "Could not determine memory usage for re1, $mem_oid_re1 ";
-		$mem_status_exit_1 =  1;
-	} else {
-		$mem_re1 = $snmp_result->{$mem_oid_re1};
-
-		if ($mem_re1 >= $mem_critical ) {
-		 	$mem_status_exit_1 =  2;
-		} elsif ($mem_re1 >= $mem_warning) {
-		 	$mem_status_exit_1 =  1;
-		} elsif (($mem_re1 < $mem_critical) && ($mem_re1 < $mem_warning)) {
-		 	$mem_status_exit_1 =  0;
+	if($re_count == 2) {
+		if ((!defined($snmp_result->{$mem_oid_re1})) || ($snmp_result->{$mem_oid_re1} eq "noSuchInstance") || ($snmp_result->{$mem_oid_re1} eq "noSuchObject")) {
+			$mem_status_str_1 =  "Could not determine memory usage for re1, $mem_oid_re1 ";
+			$mem_status_exit_1 =  1;
 		} else {
-			$mem_status_str_1 = " Unknown: Something went wrong.... ";
-		 	$mem_status_exit_1 =  3;
+			$mem_re1 = $snmp_result->{$mem_oid_re1};
+
+			if ($mem_re1 >= $mem_critical ) {
+				$mem_status_exit_1 =  2;
+			} elsif ($mem_re1 >= $mem_warning) {
+				$mem_status_exit_1 =  1;
+			} elsif (($mem_re1 < $mem_critical) && ($mem_re1 < $mem_warning)) {
+				$mem_status_exit_1 =  0;
+			} else {
+				$mem_status_str_1 = " Unknown: Something went wrong.... ";
+				$mem_status_exit_1 =  3;
+			}
 		}
 	}
 
 
-	# Now evaluate results
-	$mem_status_str = "Memory usage RE0:".$mem_re0 ."% ". $mem_status_str_0 ."RE1:".$mem_re1 ."%". $mem_status_str_1;
-	$mem_perf_data = "memory_re0 = $mem_re0%, memory_re1 = $mem_re1%";
+	if($re_count == 2) {
+		# Now evaluate results for systems with 2 REs
+		$mem_status_str = "Memory usage RE0:".$mem_re0 ."% ". $mem_status_str_0 ."RE1:".$mem_re1 ."%". $mem_status_str_1;
+		$mem_perf_data = "memory_re0 = $mem_re0%, memory_re1 = $mem_re1%";
 	
-	if ($mem_status_exit_0 > $mem_status_exit_1) {
-		$mem_status_exit =  $mem_status_exit_0;
+		if ($mem_status_exit_0 > $mem_status_exit_1) {
+			$mem_status_exit =  $mem_status_exit_0;
+		} else {
+			$mem_status_exit =  $mem_status_exit_1;
+		}
 	} else {
-		$mem_status_exit =  $mem_status_exit_1;
+		# Now evaluate results for systems with 1 RE
+		$mem_status_str = "Memory usage RE0:".$mem_re0 ."% ". $mem_status_str_0;
+		$mem_perf_data = "memory_re0 = $mem_re0%";
+	
+		$mem_status_exit =  $mem_status_exit_0;
 	}
 
 }
@@ -286,7 +353,7 @@ sub analyze_cpu {
 	my $cpu_re0 = 0;
 	my $cpu_re1 = 0;
 
-	if ((!defined($snmp_result->{$cpu_oid_re0})) || ($snmp_result->{$cpu_oid_re0} eq "noSuchInstance")) {
+	if ((!defined($snmp_result->{$cpu_oid_re0})) || ($snmp_result->{$cpu_oid_re0} eq "noSuchInstance") || ($snmp_result->{$cpu_oid_re0} eq "noSuchObject")) {
 		$cpu_status_str_0 = " Could not determine cpu usage for re0, $cpu_oid_re0 ";
 		$cpu_status_exit_0 =  1;
 	} else {
@@ -304,57 +371,71 @@ sub analyze_cpu {
 	}
 
 
-	if ((!defined($snmp_result->{$cpu_oid_re1})) || ($snmp_result->{$cpu_oid_re1} eq "noSuchInstance")) {
-		$cpu_status_str_1 =  "Could not determine cpu usage for re1, $cpu_oid_re1 ";
-		$cpu_status_exit_1 =  1;
-	} else {
-		$cpu_re1 = $snmp_result->{$cpu_oid_re1};
-		$cpu_status_exit_1 =  0;
+	if($re_count == 2) {
+		if ((!defined($snmp_result->{$cpu_oid_re1})) || ($snmp_result->{$cpu_oid_re1} eq "noSuchInstance") || ($snmp_result->{$cpu_oid_re1} eq "noSuchObject")) {
+			$cpu_status_str_1 =  "Could not determine cpu usage for re1, $cpu_oid_re1 ";
+			$cpu_status_exit_1 =  1;
+		} else {
+			$cpu_re1 = $snmp_result->{$cpu_oid_re1};
+			$cpu_status_exit_1 =  0;
 
-		if ($cpu_re1 >= $cpu_critical ) {
-                        $cpu_status_exit_1 =  2;
-			$cpu_status_str_0 .= "Criticaly high load on RE1. ";
-                } elsif ($cpu_re1 >= $cpu_warning) {
-                        $cpu_status_exit_1 =  1;
-			$cpu_status_str_1 .= "Warning high load on RE1. ";
-                }
+			if ($cpu_re1 >= $cpu_critical ) {
+				$cpu_status_exit_1 =  2;
+				$cpu_status_str_0 .= "Criticaly high load on RE1. ";
+			} elsif ($cpu_re1 >= $cpu_warning) {
+				$cpu_status_exit_1 =  1;
+				$cpu_status_str_1 .= "Warning high load on RE1. ";
+			}
 
+		}
 	}
 
-	if ((!defined($snmp_result->{$cpu_redundancy_state_oid_re0})) || ($snmp_result->{$cpu_redundancy_state_oid_re0} eq "noSuchInstance")) {
-		$cpu_status_str_1 .=  "Could not determine redundance state for for re0 ";
-		$cpu_status_exit_1 =  1;
-	}elsif (($snmp_result->{$cpu_redundancy_state_oid_re0} != 2) && ($snmp_result->{$cpu_redundancy_state_oid_re0} != 4)) {
-		# 1 = unknow, 2 = master, 3 = backup, 4 = disabled
-		$cpu_status_str_0 .=  "RE0 is not the Master RE ";
-		$cpu_status_exit_0 =  1;
+	# redundant state only exists on systems capable of having more than one RE
+	if($re_count == 2) {
+		if ((!defined($snmp_result->{$cpu_redundancy_state_oid_re0})) || ($snmp_result->{$cpu_redundancy_state_oid_re0} eq "noSuchInstance") || ($snmp_result->{$cpu_redundancy_state_oid_re0} eq "noSuchObject")) {
+			$cpu_status_str_1 .=  "Could not determine redundance state for re0 ";
+			$cpu_status_exit_1 =  1;
+		}elsif (($snmp_result->{$cpu_redundancy_state_oid_re0} != 2) && ($snmp_result->{$cpu_redundancy_state_oid_re0} != 4)) {
+			# 1 = unknow, 2 = master, 3 = backup, 4 = disabled
+			$cpu_status_str_0 .=  "RE0 is not the Master RE ";
+			$cpu_status_exit_0 =  1;
+		}
 	}
 
-
-	# Now evaluate results
-	$cpu_status_str = "CPU usage RE0:".$cpu_re0 ."% ". $cpu_status_str_0 ."RE1:".$cpu_re1 ."%". $cpu_status_str_1;
-	$cpu_perf_data = "cpu_re0 = $cpu_re0%, cpu_re1 = $cpu_re1%";
+	if($re_count == 2) {
+		# Now evaluate results for systems with 2 REs
+		$cpu_status_str = "CPU usage RE0:".$cpu_re0 ."% ". $cpu_status_str_0 ."RE1:".$cpu_re1 ."%". $cpu_status_str_1;
+		$cpu_perf_data = "cpu_re0 = $cpu_re0%, cpu_re1 = $cpu_re1%";
 	
-	if ($cpu_status_exit_0 > $cpu_status_exit_1) {
-		$cpu_status_exit =  $cpu_status_exit_0;
+		if ($cpu_status_exit_0 > $cpu_status_exit_1) {
+			$cpu_status_exit =  $cpu_status_exit_0;
+		} else {
+			$cpu_status_exit =  $cpu_status_exit_1;
+		}
 	} else {
-		$cpu_status_exit =  $cpu_status_exit_1;
+		# Now evaluate results for systems with 1 RE
+		$cpu_status_str = "CPU usage RE0:".$cpu_re0 ."% ". $cpu_status_str_0;
+		$cpu_perf_data = "cpu_re0 = $cpu_re0%";
+	
+		$cpu_status_exit =  $cpu_status_exit_0;
 	}
-
 }
 
 
 sub print_help() {
 
 	print "Juniper MX480 routing engine probe nagios plugin
-	usage:   $0 -H hostname -C community [-m <memory_usage_warning>] [-M <memory_usage_critical>] [-t <temperature_warning>] [-T <temperature_critical>]  [-l <cpu_load_warning>] [-L <cpu_load_critical>]
-	example: $0 -H cr1.vantx1 -C secret -m 75 -M 85 -t 50 -T 55 -l 75 -L 90
+	usage:   $0 -H hostname -C community [-r <1|2>] [-m <memory_usage_warning>] [-M <memory_usage_critical>] [-t <temperature_warning>] [-T <temperature_critical>]  [-l <cpu_load_warning>] [-L <cpu_load_critical>]
+	example: $0 -H cr1.vantx1 -C secret -r 2 -m 75 -M 85 -t 50 -T 55 -l 75 -L 90
 
-	[-h]    : Print this message
+	[-h]    :  Print this message
 
 	[-H]   	:  FQDN or IP address of host to poll
 
 	[-C] 	:  SNMP community.
+
+	[-r]  	:  routing engine count ( 1 .. 2)
+		:  default is auto-detect (1 for MX5/10/40/80, 2 for MX240/480/960);
 
 	[-t]  	:  temperature threshold for warnings in celcius
 		:  default is $default_temp_warning;
