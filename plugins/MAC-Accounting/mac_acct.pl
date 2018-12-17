c0cd3830b3398b193e3e21b074bd41dad9aa0037
Craig Tomkow <ctomkow@gmail.com>
2018-12-17 10:43:36 -0800

    refactored perl/mac-acct.pl into mac_acct.pl. it supports being called via cli (mac_acct.pl -d <device_id>) just like snmp_poller.pl. Also, pulls device SNMP info instead of a hardcoded value.

---
+++ plugins/MAC-Accounting/mac_acct.pl
@@ -0,0 +1,208 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $config_file = "config/cmdb.conf";
+
+### Import libs
+use DBI;
+use Config::Simple;
+use Getopt::Std;
+
+###################### Get config ###########################
+## This specifies where CMDB_Config.pm i
+use lib "perl/";
+
+use CMDB_Config;
+my %config = CMDB_Config::get_config($config_file);
+####################### Get config ###########################
+
+
+####################### Connect To MySQL ########################
+my $connectionInfo="DBI:mysql:database=$config{db_name};$config{db_host}:$config{db_port}";
+my $dbh = DBI->connect($connectionInfo,$config{db_user},$config{db_pass})
+    or die("Could not connect to Mysql!");
+
+
+my $snmpwalk = $config{'path_snmpwalk'};
+my $snmpget = $config{'path_snmpget'};
+my $rrdupdate = $config{'path_rrdupdate'};
+my $rrdtool = $config{'path_rrdtool'};
+my $rrddir = $config{'path_rrddir'};
+
+
+####################################################
+
+#-------------------------------------------------------------------------------
+#    Usage message
+#-------------------------------------------------------------------------------
+
+my $usage = <<"EOF";
+MAC Accounting Collector script
+usage:   $0 -d <device_id>
+example: $0 -d 23
+
+[-h]          : Print this message
+
+[-d] 	      : device_id
+
+
+Andree Toonk: andree.toonk\@bc.net
+Craig Tomkow: craig.tomkow\@bc.net
+
+EOF
+
+#-------------------------------------------------------------------------------
+# Check the usage
+#-------------------------------------------------------------------------------
+my $opt_string = 'h:d:';
+my %opt;
+getopts( "$opt_string", \%opt ) or die $usage;
+die $usage if (defined $opt{h});
+die $usage if (!defined $opt{d});
+
+my $device_id = $opt{d};
+
+my $community = undef;
+my $fqdn = undef;
+my $device = undef;
+
+my %device_info = get_device_info($device_id);
+
+$fqdn = $device_info{device_fqdn};
+$community = $device_info{snmp_ro};
+$device = $device_info{name};
+
+my $line;
+my $cli;
+my @results;
+my %mactoip;
+my %jnxMacHCInOctets;
+my %jnxMacHCInFrames;
+my %jnxMacHCOutOctets;
+my %jnxMacHCOutFrames;
+my %allmacs;
+
+#first get ARP table
+$cli = "$snmpwalk -v 2c -Onq -c $community $fqdn .1.3.6.1.2.1.4.22.1.2";
+@results = `$cli`;
+foreach $line (@results) {
+    chomp $line;
+    #.1.3.6.1.2.1.4.22.1.2.707.206.81.80.39 0:b0:c2:84:ab:0
+    # .1.3.6.1.2.1.4.22.1.2.707.206.81.80.30 0:b:5f:6b:74:c0
+
+    if ( $line =~ /\.1\.3\.6\.1\.2\.1\.4\.22\.1\.2\.(\d+)\.((\d+)\.(\d+)\.(\d+)\.(\d+))\s(\S+)/) {
+        my $ip = $2;
+        my $mac ='';
+        my @mac_short = split(/:/, $7);
+        foreach(@mac_short) {
+            if ($_ =~ /^.$/) {
+                $mac = $mac . "0$_:";
+            } else {
+                $mac = $mac . "$_:";
+            }
+        }
+        chop($mac);
+        $mactoip{$mac} = $ip;
+    } else {
+        #print "no match for $_\n";
+    }
+}
+
+# collect stats
+$cli = "$snmpwalk -v 2c -On -c $community $fqdn 1.3.6.1.4.1.2636.3.23";
+#exit;
+@results = `$cli`;
+foreach $line (@results) {
+    chomp $line;
+    #.1.3.6.1.4.1.2636.3.23.1.1.1.3.180.0.0.28.88.155.193.136 = Counter64: 11072
+
+    if ( $line =~ /\.1\.3\.6\.1\.4\.1\.2636\.3\.23\.1\.1\.1\.(\d+)\.(\d+)\.(\d+)\.(.+) = Counter64: (\d+)/) {
+        my $type = $1;
+        my $value = $5;
+        my $mac = '';
+        my @mac_decimal = split(/\./, $4);
+        foreach(@mac_decimal) {
+            $mac = $mac . sprintf("%02x", $_) .":";
+        }
+        # remove last :
+        chop($mac);
+        $allmacs{$mac} = $4;
+
+        if ($type == 3) {
+            $jnxMacHCInOctets{$mac} = $value;
+        }
+        elsif ($type == 4) {
+            $jnxMacHCInFrames{$mac} = $value;
+        }
+        elsif ($type == 5) {
+            $jnxMacHCOutOctets{$mac} = $value;
+        }
+        elsif ($type == 6) {
+            $jnxMacHCOutFrames{$mac} = $value;
+        }
+
+        #print "type $type mac is $mac\n";
+    }
+}
+
+
+while ( my $mac = each(%allmacs) ) {
+    ## RRD Stuff
+    #first check if rrd file exist, if not create
+    my $ip = $mactoip{$mac};
+    #print "$mac == $ip\n";
+    if (! $ip) {
+        #print "could not find IP for mac $mac\n";
+        next;
+    }
+    my $rrd_file = "MAC-ACCT_". $ip ."_device_$fqdn.rrd";
+    $rrd_file =~ s/([\$\#\@\\\/\s])/-/g;
+    create_rrd_archive($rrd_file) if ! -e "$rrddir/$rrd_file";
+    my $update = "N:$jnxMacHCInOctets{$mac}:$jnxMacHCOutOctets{$mac}:$jnxMacHCInFrames{$mac}:$jnxMacHCOutFrames{$mac}";
+    $cli = "$rrdupdate \"$rrddir/$rrd_file\" $update";
+    system($cli);
+}
+
+############################ Below are all the sub routines #########################
+
+sub create_rrd_archive {
+    # search and replace special chars
+    my $interface_file_name = shift;
+    my $variable_64bit = "18446744073709551616";
+    my $max = $variable_64bit;
+    #special chars replaced by a -, unix doesnt like / in filename
+    #64 bits max is 18446744073709551616
+    my $cmd = " $rrdtool create \'$rrddir/$interface_file_name\' \\
+      DS:INOCTETS:COUNTER:600:0:1.25000000000e+09 \\
+      DS:OUTOCTETS:COUNTER:600:0:1.25000000000e+09 \\
+      DS:INUCASTPKTS:COUNTER:600:0:U \\
+      DS:OUTUCASTPKTS:COUNTER:600:0:U \\
+      RRA:AVERAGE:0.5:1:600 \\
+      RRA:AVERAGE:0.5:6:700 \\
+      RRA:AVERAGE:0.5:24:775 \\
+      RRA:AVERAGE:0.5:288:797 \\
+      RRA:MAX:0.5:1:600 \\
+      RRA:MAX:0.5:6:700 \\
+      RRA:MAX:0.5:24:775 \\
+      RRA:MAX:0.5:288:797 ";
+    system `$cmd`;
+    # print "$cli\n";
+
+}
+
+sub get_device_info {
+    my $dev_id = shift;
+    my %info;
+    my $query = "SELECT name, snmp_ro, device_fqdn FROM Devices where device_id = '$dev_id' " ;
+    my $sth = $dbh->prepare($query);
+    $sth->execute() or die "Couldn't execute statement: " . $sth->errstr;
+    while (my @data = $sth->fetchrow_array()) {
+        $info{name} = $data[0];
+        $info{snmp_ro} = $data[1];
+        $info{device_fqdn} = $data[2];
+    }
+    $sth->finish();
+    return %info;
+}
